cb1e83fe2d2c6140752c159b9b40f1f9b42c45a6
ras-24 <siregar.riyando@gmail.com>
2024-02-23 03:21:06 +0800

    picking a winner

---
+++ src/lottery.js
@@ -0,0 +1,10 @@
+import web3 from "./web3";
+
+const address = "YOUR_ADDRESS";
+// remember to change this to your Contract Deployed Address!
+
+const abi = // [YOUR_ABI]
+// remember to change this to your Contract ABI!
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new web3.eth.Contract(abi, address);
